d7330acc3a9aa8a28d8ecef2f2504c894e72a3a1
Aron Wolf <aronwolf90@gmail.com>
2024-12-02 00:38:18 +0100

    fix icon style

--- app/helpers/breadcrumbs_helper.rb
+++ app/helpers/breadcrumbs_helper.rb
@@ -1,9 +1,9 @@
 module BreadcrumbsHelper
   def breadcrumb_icon(first)
     if first
-      icon "home"
+      icon "home", class: "w-3 h-3 me-2.5"
     else
-      icon "chevron-right"
+      icon "chevron-right", class: "w-3 h-3"
     end
   end
 end
