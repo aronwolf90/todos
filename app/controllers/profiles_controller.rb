26125d1b7265a2c7add9dbc412b264b4189de443
Aron Wolf <aronwolf90@gmail.com>
2025-03-27 00:24:12 +0100

    fix: add home to profile breadcrumps menu

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,6 @@
 class ProfilesController < ApplicationController
+  before_action :set_breadcrumbs, only: %i[ show ]
+
   def show
   end
 
@@ -7,4 +9,11 @@ class ProfilesController < ApplicationController
 
     redirect_to root_path, status: :see_other, notice: "All your data was successfully destroyed."
   end
+
+  private
+    def set_breadcrumbs
+      @breadcrumbs = [
+        { title: "Home", path: lists_path }
+      ]
+    end
 end
