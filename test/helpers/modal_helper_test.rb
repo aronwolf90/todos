65103f5cd77f50792a33a1db6a026509479487fd
Aron Wolf <aronwolf90@gmail.com>
2025-06-01 20:38:57 +0200

    use helper in test

--- test/helpers/modal_helper_test.rb
+++ test/helpers/modal_helper_test.rb
@@ -1,12 +1,12 @@
 require "test_helper"
 
 class ModalHelperTest < ActionView::TestCase
-  test "modal_close_button returns resume_historical_location link on hotwire native app" do
+  test "modal_close_button returns turbo_resume_historical_location_url link on hotwire native app" do
     request.user_agent += ";overflow-menu"
 
     html = controller.helpers.modal_close_button
 
-    assert_includes html, "resume_historical_location"
+    assert_includes html, turbo_resume_historical_location_url
     assert_includes html, "<a"
   end
 
