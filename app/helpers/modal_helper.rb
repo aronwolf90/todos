fbc0d8856ea9881f7eeae114fe88262af42f7c06
Aron Wolf <aronwolf90@gmail.com>
2025-07-26 00:53:34 +0200

    adjust footer for the modals

--- app/helpers/modal_helper.rb
+++ app/helpers/modal_helper.rb
@@ -1,6 +1,6 @@
 module ModalHelper
   def modal_footer
-    content_tag :div, class: "bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6" do
+    content_tag :div, class: "bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6 native:py-0" do
       yield
     end
   end
